ac13c0e6b126ef3990916cd1658dfbaf3b1807b0
nguyenminn <85283735+nguyenminn@users.noreply.github.com>
2021-06-03 16:03:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const CONFIG = {
-    introTitle: 'Babe à!',
+const CONFIG = { 27-08 <3
+    introTitle: 'Gửi anh!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,8 +12,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Biết mà 🥰. Love you to the moon and back 😘😘',
+    messDesc: 'Love you 3000',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
